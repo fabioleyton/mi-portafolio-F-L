aaede04fc62c826a544391ef89f86b82b5578a6d
fabio <faavendano0@soy.sena.edu.co>
2025-08-10 17:52:00 -0500

    añadido hoja de vida y proyectos

--- app.py
+++ app.py
@@ -28,7 +28,37 @@ proyectos = [
         "descripcion": "creacion de mi primer portafolio con mi experiencia como desarrollador.",
         "tecnologias": "python, flask",
         "link": "https://github.com/fabioleyton/mi-portafolio-F-L.git",
-    }
+    },
+    {
+        "nombre": "divicion de numeros para planillas",
+        "descripcion": "el objetivo es dividir los numeros en partes desiguales pero que la suma de el mismo numero total",
+        "tecnologias": "php, html, css, JS",
+        "link": "https://github.com/fabioleyton/divicion_nu_planilla.git",
+    },
+    {
+        "nombre": "peticiones",
+        "descripcion": "se crea una lista de peticiones con un contador de fecha para tener el control de las peticiones y terminan automaticamente despues de un tiempo determinado",
+        "tecnologias": "php, html, css",
+        "link": "https://github.com/fabioleyton/peticiones.git",
+    },
+    {
+        "nombre": "lector de archivos ESCEL",
+        "descripcion": "lector de archivos EXCEL, se refleja sin formato en pantalla",
+        "tecnologias": "php, html, css",
+        "link": "https://github.com/fabioleyton/lector_excel.git",
+    },
+    {
+        "nombre": "proyecto SENA",
+        "descripcion": "proyecto de inventarios donde se puede ingresar, productos, personas y usuarios para el control de inventarios de los almacenes",
+        "tecnologias": "php, html, css, js",
+        "link": "https://github.com/yerermid/inventarios.git",
+    },
+    {
+        "nombre": "PAGINA VENTA DE VINOS",
+        "descripcion": "pagina web de vinos",
+        "tecnologias": "html, css",
+        "link": "https://github.com/fabioleyton/vinos-leyton.git",
+    },
 ]
 
 # Definimos una ruta (endpoint) para la URL principal ("/").
